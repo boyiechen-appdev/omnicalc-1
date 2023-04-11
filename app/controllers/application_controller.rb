869ccf428dcb184f7514e95744f5f239901a4523
boyiechen <boyie@uchicago.edu>
2023-04-11 03:35:11 +0000

    learn cool form action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
+  def blank_squre_form
+    render({ :template => "calculation_templates/square_form.html.erb" })
+  end
 
+  def calculate_square
+    render({ :template => "calculation_templates/square_results.html.erb" })
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get("/square/new", { :controller => "application", :action => "blank_squre_form" })
 
+  get("/square/results", { :controller => "application", :action => "calculate_square" })
 end
