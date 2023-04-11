a1ba6d09f4435556489a81008ffd7de8815902f2
boyiechen <boyie@uchicago.edu>
2023-04-11 04:06:23 +0000

    finish sqrt

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,20 @@ class ApplicationController < ActionController::Base
     render({ :template => "calculation_templates/square_form.html.erb" })
   end
 
+  def blank_square_root_form
+    render({ :template => "calculation_templates/square_root_form.html.erb" })
+  end
+
   def calculate_square
     @num = params.fetch("elephant").to_f
     @square_of_num = @num ** 2
     render({ :template => "calculation_templates/square_results.html.erb" })
+  end
+
+  def calculate_square_root
+    @num = params.fetch("elephant").to_f
+    @square_root_of_num = @num ** 0.5
+    render({ :template => "calculation_templates/square_root_results.html.erb" })
   end
 
   def calculate_random
