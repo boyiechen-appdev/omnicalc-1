e6f8bb8df8b6808889e22ee821159f12fa252e99
boyiechen <boyie@uchicago.edu>
2023-04-11 04:36:35 +0000

    finish random

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,18 @@ class ApplicationController < ActionController::Base
     render({ :template => "calculation_templates/square_root_form.html.erb" })
   end
 
+  def blank_payment_form
+    render({ :template => "calculation_templates/payment_form.html.erb" })
+  end
+
+  def blank_random_form
+    # @lower = params.fetch("user_min").to_f
+    # @upper = params.fetch("user_max").to_f
+    # @result = rand(@lower..@upper)
+
+    render({ :template => "calculation_templates/random_form.html.erb" })
+  end
+
   def calculate_square
     @num = params.fetch("elephant").to_f
     @square_of_num = @num ** 2
@@ -17,6 +29,16 @@ class ApplicationController < ActionController::Base
     @num = params.fetch("elephant").to_f
     @square_root_of_num = @num ** 0.5
     render({ :template => "calculation_templates/square_root_results.html.erb" })
+  end
+
+  def calculate_payment
+    @apr = params.fetch("user_apr").to_f
+    @years = params.fetch("user_years").to_i
+    @pv = params.fetch("user_pv").to_f
+
+    @result = (@pv * @apr) / (1 - (1 + @apr) ** (-@years))
+
+    render({ :template => "calculation_templates/payment_results.html.erb" })
   end
 
   def calculate_random
